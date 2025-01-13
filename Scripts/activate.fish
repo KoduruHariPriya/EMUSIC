e17af116e2c15bb661cefe734876ac4f6d8f570c
KoduruHariPriya <haripriyakoduru039@gmail.com>
2025-01-13 11:24:52 +0530

    Add files via upload

--- Scripts/activate.fish
+++ Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "C:\Users\harip\OneDrive\Desktop\EMUSIC\EMUSIC"
+set -gx VIRTUAL_ENV "C:\Users\harip\OneDrive\Desktop\EMUSIC"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/Scripts" $PATH
